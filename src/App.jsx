50be763ed818601aa2041172cb89943f42e0d996
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2025-09-26 23:44:38 +0530

    add Lenis in my website for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React , {useState, useEffect} from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Loader from './components/Loader/Loader.jsx'
@@ -6,10 +6,27 @@ import Navbar from './components/Navbar.jsx'
 import About from './pages/About.jsx'
 import Projects from './pages/Projects.jsx' 
 import Contact from './pages/Contact.jsx'
-
+import Lenis from '@studio-freight/lenis'
 const App = () => {
   const [showNavbar, setShowNavbar] = useState(false);
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 3,
+      smooth: true,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
   return (
     <>  
    {showNavbar && <Navbar closeNavbar={() => setShowNavbar(false)} />}
@@ -25,7 +42,6 @@ const App = () => {
           <Route path="/contact" element={<Contact openNavbar={() => setShowNavbar(true)} />} />
       </Routes> 
     </>
-
   )
 }
 
